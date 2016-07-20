93e64367ff15d9956cdb1dbae520e3aea1d0c055
Pierre Criulanscy <pcriulan@gmail.com>
2016-07-20 22:11:01 +0200

    Debug is set via env_var

--- malanimals/heroku_settings.py
+++ malanimals/heroku_settings.py
@@ -3,9 +3,6 @@ import dj_database_url
 import os
 
 DATABASES['default']['CONN_MAX_AGE'] = 500
-DEBUG = False
-
-TEMPLATE_DEBUG = DEBUG
 
 DATABASES['default'] = dj_database_url.config()
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
